de14609a4d81f083198c0a4a7b7819f9533fd67f
Christine <christine@johnsonwebapps.com>
2011-03-04 16:05:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plastix2::Application.config.secret_token = '8ca91cd7e976ebe68095aab93bbcc83d6797e762a1d396076029cafa64116e97a606bf5e502e3c29e84faf38818c473d4b0143864173abe3616e9ce04bc4f01b'
