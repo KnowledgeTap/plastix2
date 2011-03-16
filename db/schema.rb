d448f289288e811052a638e3c8e287efa54aebc7
Christine <christine@johnsonwebapps.com>
2011-03-15 20:53:42 -0400

    Fixed product category id change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110307200311) do
+ActiveRecord::Schema.define(:version => 20110315232214) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20110307200311) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.text     "description"
   end
 
 end
