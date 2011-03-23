d654678b0f769bbeaba8b8fae06a45f891065e90
Christine <christine@johnsonwebapps.com>
2011-03-23 14:15:55 -0400

    Add Custom Field

--- db/migrate/20110323171713_add_custom1check_to_products.rb
+++ db/migrate/20110323171713_add_custom1check_to_products.rb
@@ -1,6 +1,6 @@
 class AddCustom1checkToProducts < ActiveRecord::Migration
   def self.up
-    add_column :products, :custom1check, :tinyint
+    add_column :products, :custom1check, :int
   end
 
   def self.down
